e3c321f14ec1bd91eda7c951e65f685b16468ce3
LE (Lee) <lm61613@gmail.com>
2021-01-09 16:32:54 -0600

    parcel bundler

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import { bra } from '../sw'
+
+document.querySelector('h1').textConent = bra('Sup from SW.js');
---
+++ sw.js
@@ -0,0 +1,7 @@
+
+// Service Workers are replacing Background in Chrome MV3
+const bra = (greeting) => {
+  return `${greeting}, bra!`
+}
+
+export {bra}
