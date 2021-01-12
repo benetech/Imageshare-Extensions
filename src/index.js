1f6e49e0a5bf1039c6b86b2df57ae1fb2a465bf5
LE (Lee) <lm61613@gmail.com>
2021-01-11 20:18:16 -0600

    manifest permission added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+//Test Code
 import { greetingEdit } from '../sw'
 
 document.querySelector('h1').textContent = greetingEdit('Sup from SW.js');
+
+// Connecting to Context Menus (ei right click menu)
+
