da256f7523683a0851da7d721eacaaa3c698d3e7
LE (Lee) <lm61613@gmail.com>
2021-01-20 12:06:28 -0600

    Context menu test opporating in MV3

--- src/background.js
+++ src/background.js
@@ -1,2 +1,45 @@
-//Adapt to the service worker exectution context
-import "crx-hotreload";
+// hot reload cannot be run on service worker
+// import "crx-hotreload";
+
+// The onClicked callback function.
+function onClickHandler(info, tab) {
+
+    console.log("item " + info.menuItemId + " was clicked");
+    console.log("info: " + JSON.stringify(info));
+    console.log("tab: " + JSON.stringify(tab));
+
+}
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// Set up context menu tree at install time.
+chrome.runtime.onInstalled.addListener(function() {
+  // Create one test item for each context type.
+  var contexts = ["page","selection","link","editable","image","video",
+                  "audio"];
+  for (var i = 0; i < contexts.length; i++) {
+    var context = contexts[i];
+    var title = "Test '" + context + "' menu item";
+    var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                         "id": "context" + context});
+    console.log("'" + context + "' item:" + id);
+  }
+
+  // Create a parent item and two children.
+  chrome.contextMenus.create({"title": "Test parent item", "id": "parent"});
+  chrome.contextMenus.create(
+      {"title": "Child 1", "parentId": "parent", "id": "child1"});
+  chrome.contextMenus.create(
+      {"title": "Child 2", "parentId": "parent", "id": "child2"});
+  console.log("parent child1 child2");
+
+  // Intentionally create an invalid item, to show off error checking in the
+  // create callback.
+  console.log("About to try creating an invalid item - an error about " +
+      "duplicate item child1 should show up");
+  chrome.contextMenus.create({"title": "ErrorTests", "id": "child1"}, function() {
+    if (chrome.extension.lastError) {
+      console.log("Got expected error: " + chrome.extension.lastError.message);
+    }
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-//Test Code
-import { greetingEdit } from '../sw'
-
-document.querySelector('h1').textContent = greetingEdit('Sup from SW.js');
-
-// Connecting to Context Menus (ei right click menu)
-
+console.log("Content script has loaded via Manifest V3.");
