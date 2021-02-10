b0c7593261cd314a98dce81496577a56d09a5217
LE (Lee) <lm61613@gmail.com>
2021-02-10 10:26:38 -0600

    popup.js lists from API

--- src-chrome-mv3/popup.js
+++ src-chrome-mv3/popup.js
@@ -1,26 +1,67 @@
 
 window.addEventListener("DOMContentLoaded",
   function () {
-    console.log("test");
     const subjList = document.getElementById("subject");
+    const typeList = document.getElementById("type");
+    const accList = document.getElementById("accommodation");
+    const srcList = document.getElementById("source");
 
-        fetch(`https://imgsdev.wpengine.com/json-api/subjects/`, {
+    fetch(`https://imgsdev.wpengine.com/json-api/subjects/`, {
       method: 'GET',
     }).then(response => response.json())
-    .then (json => {
+      .then (json => {
       const subjects = json.data
-      console.log("hello from foreach subject" + subjects);
 
       subjects.forEach(element => {
         const option = document.createElement('option');
           option.innerText = element.attributes.name;
           option.value = element.id;
           subjList.prepend(option);
-        // subjList.prepend(
-        //   <option value={element.id}>{element.attributes.name}</option>
-        // )
       });
     })
+
+    fetch(`https://imgsdev.wpengine.com/json-api/types/`, {
+      method: 'GET',
+    }).then(response => response.json())
+      .then (json => {
+      const types = json.data
+
+      types.forEach(element => {
+        const option = document.createElement('option');
+          option.innerText = element.attributes.name;
+          option.value = element.id;
+          typeList.prepend(option);
+      });
+    })
+
+    fetch(`https://imgsdev.wpengine.com/json-api/accommodations/`, {
+      method: 'GET',
+    }).then(response => response.json())
+      .then (json => {
+      const accommodations = json.data
+
+      accommodations.forEach(element => {
+        const option = document.createElement('option');
+          option.innerText = element.attributes.name;
+          option.value = element.id;
+          accList.prepend(option);
+      });
+    })
+
+    fetch(`https://imgsdev.wpengine.com/json-api/sources/`, {
+      method: 'GET',
+    }).then(response => response.json())
+      .then (json => {
+      const sources = json.data
+
+      sources.forEach(element => {
+        const option = document.createElement('option');
+          option.innerText = element.attributes.name;
+          option.value = element.id;
+          srcList.prepend(option);
+      });
+    })
+
   }
 )
 
