c1810d33b961e44eebf7d8641fa17f76c2b45186
Lauren (LE) McNamara <40642931+LE0616@users.noreply.github.com>
2021-03-11 12:36:47 -0800

    check Safari notifications

--- src-safari/background.js
+++ src-safari/background.js
@@ -87,7 +87,7 @@ function runAPIadvanced (selection, userSubject, userType, userAcc, userSrc) {
           } else {
           console.log(`${results.length} found for ${selection}`);
           openImageshare(newURL);
-
+//
           chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {
               type: 'notification',
