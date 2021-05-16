4f3c840834a19a5a9cf2d82a0712fbf827c60bee
LE (Lee) <lm61613@gmail.com>
2021-05-16 15:59:22 -0500

    old popup files removed

--- src-safari/popup.js
+++ src-safari/popup.js
@@ -1,73 +1,80 @@
 window.addEventListener("DOMContentLoaded",
 function () {
+//Version 2: With combined buttons for selection or input
 
 //GET search buttons
 const stSearchButton = document.getElementById("standard-search");
 const advSearchButton = document.getElementById("advanced-search");
-const stInputButton = document.getElementById("standard-search-input");
-const advInputButton = document.getElementById("advanced-search-input");
+const searchInput = document.getElementById("search");
 
-//Run search from popup with selection
+//GET user input
+let userSearch = searchInput.value;
+console.log(`userSearch in input BEFORE selection added as value: ${userSearch} `)
+
+//Is there a selection object present?
+//Send a message to index to get selection
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {type: 'selection'}, function(response) {
+
+    console.log(response)
+    //response may indicate no selection found
+    if (response === false) {
+
+      //Else leave blank and prompt to input a search term
+      //switch views from 1 to 2
+      // [].forEach.call(els, function (el) {...});
+      [].forEach.call(document.getElementsByClassName("view-one"), function(item) {item.style.display = "none";});
+      [].forEach.call(document.getElementsByClassName("view-two"), function(item) {item.style.display = "block";});
+    } else {
+      //If so populate to input value
+      searchInput.value = response
+      userSearch = searchInput.value;
+      console.log(`userSearch in input AFTERS selection added as value: ${userSearch}`)
+    }
+  });
+});
+
+
+// //Define selection as input's value
+// let userSearch = searchInput.value;
+// console.log(`userSearch in input after selection added as value: ${userSearch} `) //empty???
+
+//Run search from popup
 stSearchButton.addEventListener("click",
  function () {
   console.log("Standard Search button clicked");
-  //Send a message to content script to get selection
+
+  //check input for new value
+  userSearch = searchInput.value;
+
+  //send search request and selection to index
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {type: 'search', subtype: 'standard'}, function(response) {
-      //response may indicate no selection found
-      // console.log(response);
-      if (response === 'run input') {
-        //switch views from 1 to 2
-        document.getElementById("view-one").style.display = "none";
-        document.getElementById("view-two").style.display = "block";
-      }
+    chrome.tabs.sendMessage(tabs[0].id, {type: 'search-only', subtype: 'standard', selection: userSearch}, function(response) {
+      console.log(response)
+
     });
   });
 
- }
-);
+
+ })
 
 advSearchButton.addEventListener("click",
  function () {
   console.log("Advanced Search button clicked");
 
-  //Send a message to content script to get selection
+  //check input for new value
+  userSearch = searchInput.value;
+
+  //send search request and selection to index
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {type: 'search', subtype: 'advanced'}, function(response) {
-      //response may indicate no selection found
-      // console.log(response);
-      if (response === 'run input') {
-        //switch views from 1 to 2
-        document.getElementById("view-one").style.display = "none";
-        document.getElementById("view-two").style.display = "block";
-      }
+    chrome.tabs.sendMessage(tabs[0].id, {type: 'search-only', subtype: 'advanced', selection: userSearch}, function(response) {
+      console.log(response)
+
     });
   });
 
- }
-);
+ });
 
-// Run search from popup with input
-stInputButton.addEventListener("click",
- function () {
-  console.log("Standard Search button clicked");
-  const searchInput = document.getElementById("search");
-  let userSearch = searchInput.value;
-  //Send a message to background to run
-  console.log(userSearch);
-  chrome.runtime.sendMessage({type: 'input', subtype: 'standard', selection: userSearch})
 
- }
-);
 
-advInputButton.addEventListener("click",
- function () {
-  console.log("Advanced Search button clicked");
-  const searchInput = document.getElementById("search");
-  let userSearch = searchInput.value;
-  //Send a message to background to run
-  console.log(userSearch);
-  chrome.runtime.sendMessage({type: 'input', subtype: 'advanced', selection: userSearch})
- }
-);
 })
