3f1fd6d09b3340f0d6a8a271d446789e1c9aa7f6
Job van Achterberg <mail@3ode.nl>
2021-05-26 20:02:58 +0200

    Fix storage quota exceeded error

--- src/common/settings.js
+++ src/common/settings.js
@@ -1,23 +1,23 @@
 import browser from 'get-browser';
 
 export const getActiveTabSetting = () => new Promise(resolve => {
-    browser.storage.sync.get(['active'], result => resolve(!!result.active));
+    browser.storage.local.get(['active'], result => resolve(!!result.active));
 });
 
 export const getSettings = () => new Promise(resolve => {
-  browser.storage.sync.get(['settings'], result => resolve(result.settings));
+  browser.storage.local.get(['settings'], result => resolve(result.settings));
 });
 
 export const getStoredApiOptions = async () => {
   return new Promise(resolve => {
-      browser.storage.sync.get(['options'], result => resolve(result.options));
+      browser.storage.local.get(['options'], result => resolve(result.options));
   });
 };
 
 export const storeApiOptions = async options => {
   return new Promise(resolve => {
       options.timeStamp = new Date().getTime();
-      browser.storage.sync.set({
+      browser.storage.local.set({
           'options': options
       }, resolve);
   })
@@ -25,7 +25,7 @@ export const storeApiOptions = async options => {
 
 export const getStoredUserSettings = async () => {
   return new Promise(resolve => {
-      browser.storage.sync.get(['settings'], result => {
+      browser.storage.local.get(['settings'], result => {
           if (result.settings === undefined) {
               return resolve({
                   subject: undefined,
@@ -42,7 +42,7 @@ export const getStoredUserSettings = async () => {
 export const storeUserSettings = async (subject, type, accommodation, source, setActiveTab) => {
   const timeStamp = new Date().getTime();
   return new Promise(resolve => {
-    browser.storage.sync.set({
+    browser.storage.local.set({
         'settings': {
             'subject': subject ? subject.value : 0,
             'type': type ? type.value : 0,
